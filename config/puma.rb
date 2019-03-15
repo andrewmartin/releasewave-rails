8d7ff02e40a930292a1f305e46815a3798f5884d
Andrew Martin <hello@andrewmart.in>
2019-03-15 00:36:11 -0700

    adding shared path for ump

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
+# configured with ansible
+shared_dir = "/var/apps/releasewave-rails/shared/tmp"
 
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
