461017f836eec73f97383d620b6bdc1470ecd8fd
Andrew Martin <hello@andrewmart.in>
2019-03-15 00:09:45 -0700

    adding puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,6 @@
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
