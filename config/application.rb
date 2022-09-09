09a039b0dc38c6d0d8de839fbb81fe1d71624941
Andrew Martin <hello@andrewmart.in>
2022-09-09 12:15:38 -0700

    add cors

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,9 +13,9 @@ module ReleasewaveRails
     config.action_controller.forgery_protection_origin_check = false
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http:\/\/localhost:(.*)', 'https://releasewave.com', 'https://www.releasewave.com', /\Ahttp:\/\/localhost:(.*)\z/
+        origins 'http:\/\/localhost:(.*)', "https://releasewave.com", "https://www.releasewave.com", /\Ahttp:\/\/localhost:(.*)\z/, "https://releasewave-react.vercel.app", /\Ahttp(.*):\/\/(.*).ngrok.io(.*)\z/
 
-        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :patch, :options, :head], :expose  => ['uid', 'client', 'expiry', 'access-token', 'token-type']
+        resource "*", :headers => :any, :methods => [:get, :post, :delete, :put, :patch, :options, :head], :expose => ["uid", "client", "expiry", "access-token", "token-type"]
       end
     end
 
