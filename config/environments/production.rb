2a51432ea7ed635cd1614888210a02c3d213929e
Andrew Martin <hello@andrewmart.in>
2022-10-09 15:24:07 -0700

    starting storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.active_storage.service = :amazon
+
   config.paperclip_defaults = {
     storage: :s3,
-    s3_host_name: ENV.fetch('S3_HOST_NAME'),
+    s3_host_name: ENV.fetch("S3_HOST_NAME"),
     preserve_files: true,
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET'),
-      access_key_id: ENV.fetch('S3_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('S3_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('S3_REGION'),
-   }
+      bucket: ENV.fetch("S3_BUCKET"),
+      access_key_id: ENV.fetch("S3_ACCESS_KEY_ID"),
+      secret_access_key: ENV.fetch("S3_SECRET_ACCESS_KEY"),
+      s3_region: ENV.fetch("S3_REGION"),
+    },
   }
 
   # Code is not reloaded between requests.
@@ -23,7 +25,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
@@ -33,7 +35,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -64,7 +66,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -93,9 +95,9 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
